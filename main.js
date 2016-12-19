3a3a575f9e713c1d4b4318302232da9079a2b56f
Tackyou <e970501e@opayq.com>
2016-12-19 03:45:01 +0100

    new async.min.js url

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @version        0.7.1
 // @include        http://steamcommunity.com/id/*/inventory*
 // @include        http://steamcommunity.com/profiles/*/inventory*
-// @require        https://raw.github.com/caolan/async/master/lib/async.js
+// @require        https://raw.githubusercontent.com/caolan/async/master/dist/async.min.js
 // @grant          none
 // ==/UserScript==
 
